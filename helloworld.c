f8d2fce1a610eb0725ac5b45ea55c8298e473395
Juhi <31993523+j-12@users.noreply.github.com>
2019-04-15 02:31:13 +0530

    Updated with value of c
    For version control

--- helloworld.c
+++ helloworld.c
@@ -5,6 +5,8 @@ int main(){
   print("Hello World, why is this sad!")
   a=10;
   b=9;
+  c=a+b;
+  printf("Here is some reason to not be sad,the value of c is: %d",c);
   //Add code here
   return 0;
 }
